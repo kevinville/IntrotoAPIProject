039ede6655a81085499892cde0e801bca0559977
kevinville <kevinlhardyal@gmail.com>
2023-04-06 16:29:50 -0400

    Added 3rd Endpoint

---
+++ Controllers/PublicController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using WorkerSalaryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkerSalaryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicController : ControllerBase
+    {
+        private readonly SalaryDbContext _context;
+
+
+        public PublicController(SalaryDbContext context)
+        {
+            _context = context;
+        }
+
+        // api/Worker
+        [HttpGet]
+        public async Task<ActionResult<Response>> GetPublicInfo()
+        {
+            var response = new Response();
+            response.statusCode = 200;
+
+            var listofworkers = await _context.PublicInfo.ToListAsync();
+
+            response.Public = listofworkers;
+
+            if (listofworkers.Count == 0)
+            {
+                response.statusCode = 404;
+                response.statusDescription = "No workers?";
+            }
+
+            return response;
+
+        }
+    }
+}
